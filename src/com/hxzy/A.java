8a34c5a4b5d546613522f9685d8fef6c1568d0a9
Gaojian1998 <76153387+Gaojian1998@users.noreply.github.com>
2020-12-17 17:35:41 +0800

    Update A.java
    服务端修改的啊啊

--- src/com/hxzy/A.java
+++ src/com/hxzy/A.java
@@ -1,7 +1,11 @@
 package com.hxzy;
-
+//服务端修改
 public class A {
    public static void main(String[] args) {
 	System.out.println("Hello World!");
+	 int a=3;
+	 int b=4;
+	 int c=a+b;
+	 System.out.println("a+b=="+c);
 }
 }
